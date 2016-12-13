d52969665a2758be3538c0ad29004d569a64cad5
charlesrt <hello@charlesrt.co.uk>
2016-12-13 17:04:19 +0000

    dev: move example and make content agnostic

--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -6,17 +6,3 @@ $('ul.tab-nav li a').click(function(){
     $('.tab-content').not(target).addClass('js-hidden');
     $(target).removeClass('js-hidden');
 });
-
-$('.js-button-next-tab').click(function(e){
-
-    e.preventDefault();
-    var target = $(this).data("target");
-
-    window.location.href = "#example-component";    
-    $('.tab-content').not("#" + target).addClass('js-hidden');
-    $("#" +target).removeClass('js-hidden');
-    $('ul.tab-nav li a[data-target="' + target + '"]').parent().prev().find('.hod-checkmark').removeClass('js-hidden');
-    $('ul.tab-nav li a').removeClass('active');
-    $('ul.tab-nav li a[data-target="' + target + '"]').addClass('active');
-
-});
