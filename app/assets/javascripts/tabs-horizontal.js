d0f46492cd52cebe234bb76bf5df1c83d2fea2b0
Chris Hanson <chris.hanson@digital.homeoffice.gov.uk>
2017-10-04 13:39:27 +0100

    updated the horizontal
    Updated styles and change functionality, including extending to grab the hash form the url and open that tab on load.

--- app/assets/javascripts/tabs-horizontal.js
+++ app/assets/javascripts/tabs-horizontal.js
@@ -1,8 +1,33 @@
-/* horizontal-tab navigation and content show/hide */
-$('ul.tab__horizontal li a').click(function(){
-    var target = "#" + $(this).data("target");
-    $("ul.tab__horizontal li").removeClass('active');
-    $(this).parents('li').addClass('active');
-    $('.tab__content-horizontal').not(target).addClass('js-hidden');
-    $(target).removeClass('js-hidden');
+// as JS is working, add 'tabs' class, which will style tabs and allow
+// functionality
+$('.js-tabs').addClass('tabs');
+
+// hide all of the tab content for now
+$('.tab-content').hide();
+
+// check for hash in url and open that tab if its there, else show the first
+// tab and content
+var hash = window.location.hash;
+if (hash) {
+    $('.tabs a[href="' + hash +'"]').parent('li').addClass('active');
+    $(hash).show();
+} else {
+    $('.tab-content:first').show();
+    $('.tabs ul li:first').addClass('active');
+}
+
+$('.tabs a').click(function(e){
+  e.preventDefault();
+
+  var $this = $(this);
+  var currentTab = $this.attr('href');
+
+  // remove active class from nav and add to newly selected tab
+  $(".tabs li").removeClass('active');
+  $this.parent().addClass('active');
+
+  // hide all of the tab content and show newly selected then update hash in URL
+  $('.tab-content').hide();
+  $(currentTab).show();
+  history.pushState({}, '', currentTab);
 });
