b2b806a72b1cb814a22a92aadcfb2577bdc9de80
Michael <michael.owen6@homeoffice.gsi.gov.uk>
2018-01-15 12:34:35 +0000

    dev: simplify html to make consuming the component easier #97

--- app/assets/javascripts/side-menu.js
+++ app/assets/javascripts/side-menu.js
@@ -1,16 +1,6 @@
-/* tab navigation and content show/hide */
 $('ul.tab__nav li a').click(function(){
     var target = "#" + $(this).data("target");
     $("ul.tab__nav li a").removeClass('active');
-    $(this).addClass('active');
-    $('.tab__content').not(target).addClass('js-hidden');
-    $(target).removeClass('js-hidden');
-});
-
-/* grey tab navigation and content show/hide */
-$('ul.tab__nav--grey li a').click(function(){
-    var target = "#" + $(this).data("target");
-    $("ul.tab__nav-grey li a").removeClass('active');
     $(this).addClass('active');
     $('.tab__content').not(target).addClass('js-hidden');
     $(target).removeClass('js-hidden');
