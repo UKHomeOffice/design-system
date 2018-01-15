bd43e3105b6d873c3840d7f6cd445b5cb25ba1ae
Michael <michael.owen6@homeoffice.gsi.gov.uk>
2018-01-15 11:21:40 +0000

    dev: update scss and css for side tabs

--- app/assets/javascripts/side-menu.js
+++ app/assets/javascripts/side-menu.js
@@ -1,17 +1,17 @@
-/* menu navigation and content show/hide */
-$('ul.tab--nav li a').click(function(){
+/* tab navigation and content show/hide */
+$('ul.tab__nav li a').click(function(){
     var target = "#" + $(this).data("target");
-    $("ul.tab--nav li a").removeClass('active');
+    $("ul.tab__nav li a").removeClass('active');
     $(this).addClass('active');
-    $('.tab--content').not(target).addClass('js-hidden');
+    $('.tab__content').not(target).addClass('js-hidden');
     $(target).removeClass('js-hidden');
 });
 
-/* grey menu navigation and content show/hide */
-$('ul.tab--nav-grey li a').click(function(){
+/* grey tab navigation and content show/hide */
+$('ul.tab__nav--grey li a').click(function(){
     var target = "#" + $(this).data("target");
-    $("ul.tab--nav-grey li a").removeClass('active');
+    $("ul.tab__nav-grey li a").removeClass('active');
     $(this).addClass('active');
-    $('.tab--content').not(target).addClass('js-hidden');
+    $('.tab__content').not(target).addClass('js-hidden');
     $(target).removeClass('js-hidden');
 });
