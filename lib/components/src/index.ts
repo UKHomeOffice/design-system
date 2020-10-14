988724641187b1571ae4cf67fc8561dc73a60c96
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-10-14 15:46:12 +0100

    Add Alerts component to meta-package

--- lib/components/src/index.ts
+++ lib/components/src/index.ts
@@ -1,3 +1,4 @@
+export { default as Alerts } from '@hods/alerts';
 export { default as Footer } from '@hods/footer';
 export { default as Header } from '@hods/header';
 export { default as Page } from '@hods/page';
