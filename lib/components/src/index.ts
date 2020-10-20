ce6e405a976b5fcbf2b39ef83e531c28da0899e0
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-10-20 23:43:32 +0100

    Fix Alert export
    We were still exporting as `<Alerts>`.

--- lib/components/src/index.ts
+++ lib/components/src/index.ts
@@ -1,4 +1,4 @@
-export { default as Alerts } from '@hods/alerts';
+export { default as Alert } from '@hods/alert';
 export { default as Footer } from '@hods/footer';
 export { default as Header } from '@hods/header';
 export { default as Page } from '@hods/page';
