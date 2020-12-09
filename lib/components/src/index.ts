c7583393fb066e6e60832daff1cc3388b59f666d
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-12-09 21:35:29 +0000

    components: Add StatusBanner to meta-package

--- lib/components/src/index.ts
+++ lib/components/src/index.ts
@@ -2,3 +2,4 @@ export { default as Alert } from '@hods/alert';
 export { default as Footer } from '@hods/footer';
 export { default as Header } from '@hods/header';
 export { default as Page } from '@hods/page';
+export { default as StatusBanner } from '@hods/status-banner';
