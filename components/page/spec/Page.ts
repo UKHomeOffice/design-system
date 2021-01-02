8b2d676b5cec7987ece3538e073ebc19159ffe9e
Daniel A.C. Martin <github@daniel-martin.co.uk>
2021-01-02 23:36:44 +0000

    Page: Update to new interfaces & provide HTML head

--- components/page/spec/Page.ts
+++ components/page/spec/Page.ts
@@ -13,14 +13,15 @@ describe('Page', () => {
     const component = mount(h(Page, {
       accountHref: '#/my-account',
       navigation: [
-        { href: '/styles', text: 'Styles', active: true },
+        { href: '/styles', text: 'Styles' },
         { href: '/components', text: 'Components' },
         { href: '/patterns', text: 'Patterns' },
         { href: '/resources', text: 'Resources' },
         { href: '/get-involved', text: 'Get involved' }
       ],
-      title: 'Service name',
+      serviceName: 'Service name',
       signOutHref: '#/auth/sign-out',
+      title: 'Service title',
       username: 'User name',
       footerNavigation: [
         { href: '/feedback', text: 'Feedback' },
