c1cde42417708ea31c3d0487c9f2071ff129bcff
Karwai Pun <karwai.pun@digital.homeoffice.gov.uk>
2021-04-19 21:11:55 +0100

    updates to deployment guidance

--- apps/docs/src/common/pages/get-started/deploy-prototype.tsx
+++ apps/docs/src/common/pages/get-started/deploy-prototype.tsx
@@ -3,6 +3,10 @@ import { Helmet } from 'react-helmet-async';
 import { PageProps } from '@not-govuk/app-composer';
 import { A, NavigationMenu } from '@not-govuk/components';
 
+const applyPrototype7 = require('../../../../assets/images/heroku2.png').default;
+const applyPrototype8 = require('../../../../assets/images/heroku3.png').default;
+const applyPrototype9 = require('../../../../assets/images/heroku4.png').default;
+
 const Page: FC<PageProps> = ({ location }) => (
   <div className="govuk-grid-row">
     <Helmet>
@@ -32,11 +36,37 @@ const Page: FC<PageProps> = ({ location }) => (
       </h1>
       <p>One of the ways you can deploy, or publish and share your prototype with others, is through Heroku.</p>
 
-      <div className="govuk-inset-text">
-      Do not enter real user data into prototypes hosted on Heroku. If your prototype stores or collects user data, talk to a security professional about appropriate security steps you must take.
-      </div>
+        <h2>Create an app on Heroku</h2>
+        <p>1. Create a <A href="https://dashboard.heroku.com/">Heroku</A> account</p>
+        <p>2. Click on <strong>New</strong> and select <strong>Create new app</strong> </p>
+        <p>3. A prototype deployed on Heroku is called an ‘app’. Type in an app name for your prototype and choose Europe as your region. Click <strong>Create app</strong></p>
 
-      <p>Placeholder </p>
+        <div className="govuk-grid-row">
+          <div className="govuk-grid-column-full">
+          <img  src={applyPrototype7} className="confirm-desktop" style={{maxHeight: "100%"}} alt="Heroku screen showing area for creating new app"/>
+          </div>
+        </div>
+        <p></p>
+
+        <h2>Deploy your prototype</h2>
+        <p>Under deployment method, <strong>Heroku Git</strong> should be pre-selected. Follow the instructions on the page to <strong>Deploy using Heroku Git</strong></p>
+
+        <div className="govuk-grid-row">
+          <div className="govuk-grid-column-full">
+          <img  src={applyPrototype8} className="confirm-desktop" style={{maxHeight: "100%"}} alt="Heroku screen showing deployment method using Heroku Git"/>
+          </div>
+        </div>
+        <p></p>
+
+        <h2>Set a username and password</h2>
+        <p>1. After you’ve completed the last step to deploy your application, click on the <strong>Settings</strong> tab on Heroku, click on <strong>Reveal config vars</strong> and enter your username and password.</p>
+        <p>2. Once that is done, click on the <strong>Open app</strong> tab to view your prototype. It will have a url like <strong>your-prototype.herokuapp.com</strong></p>
+
+        <div className="govuk-grid-row">
+          <div className="govuk-grid-column-full">
+          <img  src={applyPrototype9} className="confirm-desktop" style={{maxHeight: "100%"}} alt="Heroku screen showing area for setting username and password"/>
+          </div>
+        </div>
     </div>
   </div>
 );
