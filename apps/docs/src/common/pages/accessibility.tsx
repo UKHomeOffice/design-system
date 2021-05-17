83bf04f23cda5004152cca58fa9613b08d82cf0a
Karwai Pun <karwai.pun@digital.homeoffice.gov.uk>
2021-05-17 17:25:06 +0100

    update accessibility main section and updates on homepage

--- apps/docs/src/common/pages/accessibility.tsx
+++ apps/docs/src/common/pages/accessibility.tsx
@@ -19,7 +19,13 @@ const Page: FC<PageProps> = ({ location }) => (
     </div>
     <div className="govuk-grid-column-three-quarters">
       <h1>Accessibility</h1>
-  <p>placeholder</p>
+      <p>Accessibility means making your website, app or service usable by as many people as possible,
+      regardless of specific access needs.</p>
+      <p>Making something accessible improves everyone’s ability to use a service, which is affected by
+      different needs and circumstances such as location, health and equipment.</p>
+      <p>Delivering accessible and inclusive services is also in line with government standards and our legal
+      responsibilities for equality and accessibility.</p>
+      <p>We will be adding more guidance in the coming weeks.</p>
     </div>
   </div>
 );
