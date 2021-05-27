723781b137e45e7a78aea04c69bc3fca396d680a
Daniel A.C. Martin <github@daniel-martin.co.uk>
2021-05-27 19:08:19 +0100

    Rename StatusBanner to ExpandableBanner

--- apps/docs/src/common/pages/components.tsx
+++ apps/docs/src/common/pages/components.tsx
@@ -7,10 +7,10 @@ import { DocsPage } from '@not-govuk/docs-components';
 const reduceToLookup = (acc: object, cur) => ({...acc, [cur.default.title]: cur});
 const storySources = [
   require('../../../../../components/alert/spec/Alert.stories.mdx'),
+  require('../../../../../components/expandable-banner/spec/ExpandableBanner.stories.mdx'),
   require('../../../../../components/footer/spec/Footer.stories.mdx'),
   require('../../../../../components/header/spec/Header.stories.mdx'),
   require('../../../../../components/page/spec/Page.stories.mdx'),
-  require('../../../../../components/status-banner/spec/StatusBanner.stories.mdx'),
   require('../../../../../components/status-message/spec/StatusMessage.stories.mdx'),
   require('../../../../../components/timeline/spec/Timeline.stories.mdx'),
   require('../../../../../components/pagination/spec/Pagination.stories.mdx')
