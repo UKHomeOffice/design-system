28a361102a840604f04f7f46d6f7e23ac93d7d0f
Daniel A.C. Martin <github@daniel-martin.co.uk>
2021-06-13 17:24:38 +0100

    Delete legacy design system

--- app/assets/javascripts/navigation.js
+++
@@ -1,9 +0,0 @@
-/* tab navigation and content show/hide */
-// Check the href of each link in the sidebar
-// $('.navbar__list-items a').click(function (e) {
-//     e.preventDefault();
-//     var target = $(this);
-//     target.parents('.navbar').find('.active').removeClass('active');
-//     target.parents('li').addClass('active');
-//     target.parents().find('h1:first').text($(this).text());
-// });
--- app/assets/javascripts/side-menu.js
+++
@@ -1,7 +0,0 @@
-$('ul.tab__nav li a').click(function(){
-    var target = "#" + $(this).data("target");
-    $("ul.tab__nav li a").removeClass('active');
-    $(this).addClass('active');
-    $('.tab__content').not(target).addClass('js-hidden');
-    $(target).removeClass('js-hidden');
-});
