83cf459f0372639eb39c7c7d053cc305f0601a3f
haggishunt56 <whatevermaan@hotmail.com>
2021-08-25 15:45:35 +0100

    improvements to sitemap code

--- apps/docs/src/common/pages/sitemap.tsx
+++ apps/docs/src/common/pages/sitemap.tsx
@@ -19,8 +19,8 @@ const Page: FC<PageProps> = ({ routes }) => {
   // Create automatic site index & section arrays
   const all = routes.map(e => ({ href: e.href, text: e.title })).sort(compare);
 
-  const getStarted = all.filter(({href}) => (
-    href.split('/')[1] === 'get-started'
+  const getStarted = all.filter(({href, text}) => (
+    href.split('/')[1] === 'get-started' && text !== 'Production'
   ));
   const styles = all.filter(({href}) => (
     href.split('/')[1] === 'styles'
@@ -29,7 +29,7 @@ const Page: FC<PageProps> = ({ routes }) => {
     href.split('/')[1] === 'patterns'
   ));
   const accessibility = all.filter(({href}) => (
-    href.split('/')[1] === 'accessibility'
+    href.split('/')[1] === 'accessibility' && text !== 'xxx'
   ));
   const getInvolved = all.filter(({href}) => (
     href.split('/')[1] === 'get-involved'
@@ -43,36 +43,26 @@ const Page: FC<PageProps> = ({ routes }) => {
 
   // Manually remove pages we don't want to show in the sitemap
   const hidden = [
-    ...getStarted.splice(getStarted.indexOf(getStarted.find(element => element.text === "Get started")), 1),
-    ...getStarted.splice(getStarted.indexOf(getStarted.find(element => element.text === "Production")), 1),
-    ...styles.splice(styles.indexOf(styles.find(element => element.text === "Styles")), 1),
-    ...components.splice(components.indexOf(components.find(element => element.text === "Components")), 1),
-    ...patterns.splice(patterns.indexOf(patterns.find(element => element.text === "Patterns")), 1),
-    ...accessibility.splice(accessibility.indexOf(accessibility.find(element => element.text === "Accessibility")), 1),
-    ...accessibility.splice(accessibility.indexOf(accessibility.find(element => element.text === "xxx")), 1),
-    ...getInvolved.splice(getInvolved.indexOf(getInvolved.find(element => element.text === "Get involved")), 1),
-    ...footer.splice(footer.indexOf(footer.find(element => element.text === "Cookies")), 1),
-    ...footer.splice(footer.indexOf(footer.find(element => element.text === "Components")), 1),
+    ...all.filter({text}) => (text === "Production"),
+    ...all.filter({text}) => (text === "xxx"),
+    ...all.filter({text}) => (text === "Cookies")
   ];
 
-  const getStartedMain = getStarted.filter(({href}) => (
-    href.split('/')[2] === undefined
-  ))[0];
-  const stylesMain = styles.filter(({href}) => (
-    href.split('/')[2] === undefined
-  ))[0];
-  const patternsMain = patterns.filter(({href}) => (
-    href.split('/')[2] === undefined
-  ))[0];
-  const accessibilityMain = accessibility.filter(({href}) => (
-    href.split('/')[2] === undefined
-  ))[0];
-  const getInvolvedMain = getInvolved.filter(({href}) => (
-    href.split('/')[2] === undefined
-  ))[0];
-  const componentsMain = components.filter(({href}) => (
-    href.split('/')[2] === undefined
-  ))[0];
+  const getStartedHeader = getStarted.filter(({href}) => ( href.split('/')[2] === undefined ))[0];
+  const stylesHeader = styles.filter(({href}) => ( href.split('/')[2] === undefined ))[0];
+  const patternsHeader = patterns.filter(({href}) => ( href.split('/')[2] === undefined ))[0];
+  const accessibilityHeader = accessibility.filter(({href}) => ( href.split('/')[2] === undefined ))[0];
+  const getInvolvedHeader = getInvolved.filter(({href}) => ( href.split('/')[2] === undefined ))[0];
+  const componentsHeader = components.filter(({href}) => ( href.split('/')[2] === undefined ))[0];
+
+  const headerPages = [
+    ...getStartedHeader,
+    ...stylesHeader,
+    ...patternsHeader,
+    ...accessibilityHeader,
+    ...getInvolvedHeader,
+    ...componentsHeader
+  ];
 
   const categorised = [
     ...getStarted,
@@ -81,6 +71,7 @@ const Page: FC<PageProps> = ({ routes }) => {
     ...accessibility,
     ...getInvolved,
     ...components,
+    ...headerPages,
     ...hidden
   ];
 
@@ -95,17 +86,17 @@ const Page: FC<PageProps> = ({ routes }) => {
         <meta name="og:description" content={description} />
       </Helmet>
       <h1>{title}</h1>
-      <h2><A href={getStartedMain.href}>{getStartedMain.text}</A></h2>
+      <h2><A href={getStartedHeader.href}>{getStartedHeader.text}</A></h2>
       <AnchorList classBlock="govuk-list" items={getStarted} />
-      <h2><A href={stylesMain.href}>{stylesMain.text}</A></h2>
+      <h2><A href={stylesHeader.href}>{stylesHeader.text}</A></h2>
       <AnchorList classBlock="govuk-list" items={styles} />
-      <h2><A href={componentsMain.href}>{componentsMain.text}</A></h2>
+      <h2><A href={componentsHeader.href}>{componentsHeader.text}</A></h2>
       <AnchorList classBlock="govuk-list" items={components} />
-      <h2><A href={patternsMain.href}>{patternsMain.text}</A></h2>
+      <h2><A href={patternsHeader.href}>{patternsHeader.text}</A></h2>
       <AnchorList classBlock="govuk-list" items={patterns} />
-      <h2><A href={accessibilityMain.href}>{accessibilityMain.text}</A></h2>
+      <h2><A href={accessibilityHeader.href}>{accessibilityHeader.text}</A></h2>
       <AnchorList classBlock="govuk-list" items={accessibility} />
-      <h2><A href={getInvolvedMain.href}>{getInvolvedMain.text}</A></h2>
+      <h2><A href={getInvolvedHeader.href}>{getInvolvedHeader.text}</A></h2>
       <AnchorList classBlock="govuk-list" items={getInvolved} />
       <h2>Footer</h2>
       <AnchorList classBlock="govuk-list" items={footer} />
