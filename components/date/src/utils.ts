5266f829ce02d21e14671fa8a102683c862f0f20
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-03-11 16:10:04 +0000

    remove verbose switch statement

--- components/date/src/utils.ts
+++ components/date/src/utils.ts
@@ -44,35 +44,7 @@ export type dayNumbers =
 30 | 
 31;
 
-export const convertFrom24HourTime = (time: number): number => {
-    switch(time){
-        case 13:
-            return 1
-        case 14:
-            return 2
-        case 15:
-            return 3
-        case 16:
-            return 4
-        case 17:
-            return 5
-        case 18:
-            return 6
-        case 19:
-            return 7
-        case 20:
-            return 8
-        case 21:
-            return 9
-        case 22:
-            return 10
-        case 23:
-            return 11
-        default:
-            return
-    }
-}
-
+export const times = [13,14,15,16,17,18,19,20,21,22,23];
 export const months = [
 		"January",
 		"February",
@@ -100,5 +72,5 @@ export const formatTimeFromISOString = (dateString: string): string => {
     const date = new Date(dateString);
     const hours = date.getHours();
     const minutes = date.getMinutes();
-    return `${hours > 12 ? convertFrom24HourTime(hours) : hours}:${minutes}${hours > 11 ? "pm" : "am"}`
+    return `${hours > 12 ? times.indexOf(hours) + 1 : hours}:${minutes}${hours > 11 ? "pm" : "am"}`
 }
