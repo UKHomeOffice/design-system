d205309535fd8583578e3c2e623041f1dfe869b2
Daniel A.C. Martin <github@daniel-martin.co.uk>
2022-04-26 17:26:00 +0100

    Storybook: Remove deprecated `knobs` package

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,7 +2,6 @@ import { createElement as h } from 'react';
 import { withA11y } from '@storybook/addon-a11y';
 import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
 import { withTests } from '@storybook/addon-jest';
-import { withKnobs } from "@storybook/addon-knobs";
 import { addDecorator, addParameters } from '@storybook/react';
 import globalDecorator from './decorators';
 import jestResults from '../.jest-results.json';
@@ -22,9 +21,6 @@ addDecorator(globalDecorator);
 
 // accessibility add-on
 addDecorator(withA11y as any);
-
-// knobs add-on
-addDecorator(withKnobs);
 
 // jest add-on
 addDecorator(
