43bf39bbb84393062d822ed3a48336386b9ffee6
tom.yems <tom.yems@digital.homeoffice.gov.uk>
2022-05-04 15:55:35 +0100

    Updated timeline component to use the DateTime component

--- components/timeline/src/Timeline.tsx
+++ components/timeline/src/Timeline.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode, createElement as h } from 'react';
 import { StandardProps, classBuilder } from '@not-govuk/component-helpers';
+import { DateTime } from '@hods/date-time';
 
 import '../assets/Timeline.scss';
 
@@ -37,7 +38,14 @@ export const Timeline: FC<TimelineProps> = ({
           <div className={classes('item')} key={e.date.getTime()}>
             <h2 className={classes('title')}>{e.action}</h2>
             {!e.by ? null : (<p className={classes('by')}> by {e.by}</p>)}
-            <p className={classes('date')}><time dateTime={e.date.toISOString()}>{e.date.toLocaleString('en-GB', { timeZone: 'UTC' })}</time></p>
+            <DateTime
+              className="hods-timeline__date"
+              ISOString={e.date.toISOString()}
+              displayTime={true}
+              displayDate={true}
+              precedence={"date"}
+              clockType={12}
+            />
             <p className={classes('description')}>{e.description}</p>
           </div>
         )) : null }
