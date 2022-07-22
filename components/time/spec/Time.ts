5c654b36a14a27d324bf04560679bb253e90aaef
ashby-s <109519556+ashby-s@users.noreply.github.com>
2022-07-22 13:13:33 +0100

    Create tests for Time component

--- components/time/spec/Time.ts
+++ components/time/spec/Time.ts
@@ -3,21 +3,61 @@ import { mount } from '@not-govuk/component-test-helpers';
 import Time from '../src/Time';
 
 describe('Time', () => {
-  const minimalProps = {
-  };
+  const minimalProps = {time:'2022-07-21T22:37:34.570Z'};
 
   describe('when given minimal valid props', () => {
-    const component = mount(h(Time, minimalProps, 'Child'));
+    const component = mount(h(Time, minimalProps));
 
-    it('renders', () => undefined);
+    it("formats time", () => {
+			expect(component.text()).toBe("11:37pm");
+    });
+    it("is wrapped in a time element", () => {
+    			expect(component.find('time').length).toBe(1);
+    });
+ });
+ 
+ 
+  describe('when given short time props', () => {
+    const component = mount(h(Time, {time:'22:37'}));
+
+    it("formats time", () => {
+			expect(component.text()).toBe("11:37pm");
+    });
+    it("is wrapped in a time element", () => {
+    			expect(component.find('time').length).toBe(1);
+    });
   });
-
-  describe('when given all valid props', () => {
+ 
+ describe('when given 24 hour prop', () => {
     const props = {
-      ...minimalProps
+      ...minimalProps,
+      clockType:24
     };
-    const component = mount(h(Time, props, 'Child'));
+    const component = mount(h(Time, props));
 
-    it('renders', () => undefined);
-  });
+    it("formats time", () => {
+			expect(component.text()).toBe("23:37");
+    });
+        it("is wrapped in a time element", () => {
+    			expect(component.find('time').length).toBe(1);
+    });
+    });
+ 
+ 
+ 
+describe('when given wrap false prop', () => {
+    const props = {
+      ...minimalProps,
+      wrap:false
+    };
+    const component = mount(h(Time, props));
+
+    it("formats time", () => {
+			expect(component.text()).toBe("11:37pm");
+    });
+        it("is NOT wrapped in a time element", () => {
+    			expect(component.find('time').length).toBe(0);
+    });
 });
+});
+
