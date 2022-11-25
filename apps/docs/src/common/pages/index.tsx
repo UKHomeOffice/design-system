75a445c6f7a4fa352195b371a7d0cb7eeed65001
tom.yems <tom.yems@digital.homeoffice.gov.uk>
2022-11-25 10:46:26 +0000

    Updated today's date

--- apps/docs/src/common/pages/index.tsx
+++ apps/docs/src/common/pages/index.tsx
@@ -23,7 +23,7 @@ const Page: FC<PageProps> = props => (
       <div className="govuk-grid-column-one-third">
         <aside>
           <h2>Updates</h2>
-          <p className="govuk-body govuk-!-font-size-16" style={{ color: '#626a6e' }}>Last updated 14 Oct 2022</p>
+          <p className="govuk-body govuk-!-font-size-16" style={{ color: '#626a6e' }}>Last updated 25 Nov 2022</p>
           <ul className="govuk-!-font-size-16">
             <li>Added <A href="/patterns/send-a-letter">designing letters guidance</A></li>
           </ul>
