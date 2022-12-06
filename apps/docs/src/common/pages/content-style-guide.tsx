a61be3c707c4d4e823570e689b55a741ad1f591d
Daniel A.C. Martin <github@daniel-martin.co.uk>
2022-12-06 18:05:12 +0000

    docs: Amendments to content style guide

--- apps/docs/src/common/pages/content-style-guide.tsx
+++ apps/docs/src/common/pages/content-style-guide.tsx
@@ -4,7 +4,7 @@ import { PageProps } from '@not-govuk/app-composer';
 import { A, NavigationMenu } from '@not-govuk/components';
 import { AZNavigation } from '@hods/components';
 
-export const title = 'Content Style Guide';
+export const title = 'Content style guide';
 const description = 'How to write content for Home Office services.';
 
 const Page: FC<PageProps> = ({ location }) => (
@@ -18,9 +18,9 @@ const Page: FC<PageProps> = ({ location }) => (
     </Helmet>
 
     <h1>{title}</h1>
-    <AZNavigation />
+    <AZNavigation id="a-z-nav" />
 
-    <h2 id="a">A</h2>
+    <h2 className="a-z-header" id="a">A</h2>
     <h3 id="abroad">abroad</h3>
     <p>‘Abroad’ and ‘overseas’ can cause confusion. Try to use plain English. For example, ‘apply from outside the UK’ or ‘working in another country’.</p>
     <h3 id="account">account</h3>
@@ -29,8 +29,9 @@ const Page: FC<PageProps> = ({ location }) => (
     <p>See guidance on abbreviations and acronyms in <A href="https://www.gov.uk/guidance/style-guide/a-to-z-of-gov-uk-style">the GOV.UK style guide</A>.</p>
     <h3 id="alter">alter</h3>
     <p>Use ‘change’ rather than ‘alter’, ‘modify’ or ‘switch’.</p>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
-    <h2 id="b">B</h2>
+    <h2 className="a-z-header" id="b">B</h2>
     <h3 id="biometrics">biometrics</h3>
     <p>When using ‘biometrics’ or ‘biometric information’ in public-facing services, explain what it means first. For example, ‘you'll be asked to provide your fingerprints and photo (biometric information)’.</p>
     <h3 id="biometric residence permit">biometric residence permit (BRP)</h3>
@@ -45,8 +46,9 @@ const Page: FC<PageProps> = ({ location }) => (
     <p>Write ‘overseas territories’ in lower case.</p>
     <h3 id="british overseas territories citizen">British overseas territories citizen</h3>
     <p>Write ‘overseas territories citizen’ in lower case.</p>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
-    <h2 id="c">C</h2>
+    <h2 className="a-z-header" id="c">C</h2>
     <h3 id="capital letters">capital letters</h3>
     <p>See guidance on capitalisation in the <A href="https://www.gov.uk/guidance/style-guide/a-to-z-of-gov-uk-style">GOV.UK style guide</A>.</p>
     <h3 id="casework">casework</h3>
@@ -59,8 +61,6 @@ const Page: FC<PageProps> = ({ location }) => (
     <p>Use ‘check’ rather than ‘verify’ or ‘validate’. For example, ‘check how long you’ve lived in the UK’.</p>
     <h3 id="clandestine">clandestine</h3>
     <p>Do not refer to people as ‘a clandestine’ or ‘clandestine entrant’. Try to be specific, like ‘a person hidden in a vehicle, ship or plane’.</p>
-    <h3 id="clandestine entrant">clandestine entrant</h3>
-    <p>Do not use in public-facing services. This is legal jargon for someone suspected of being an illegal immigrant.</p>
     <h3 id="click">click</h3>
     <p>Use ‘select’ rather than ‘click’ or ‘tap’ because not everyone uses a mouse.</p>
     <h3 id="common travel area">Common Travel Area</h3>
@@ -83,8 +83,9 @@ const Page: FC<PageProps> = ({ location }) => (
     <p>Use ‘customs’ rather than ‘secondary control’. For example, ‘customs regulations’ or ‘customs allowances’.</p>
     <h3 id="customs officer">customs officer</h3>
     <p>Use ‘Border Force officer’ rather than ‘customs officer’ unless you have a specific reason.</p>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
-    <h2 id="d">D</h2>
+    <h2 className="a-z-header" id="d">D</h2>
     <h3 id="dbs adult first">DBS adult first</h3>
     <p>Use lower case for ‘adult first’.</p>
     <h3 id="dbs adults barred list">DBS adults barred list</h3>
@@ -103,24 +104,28 @@ const Page: FC<PageProps> = ({ location }) => (
     <p>Not a synonym for ‘removal’. ‘Deportation’ is the forced removal of someone for the ‘public good’, usually after being convicted of a criminal offence.</p>
     <h3 id="disclosure and barring service">Disclosure and Barring Service (DBS)</h3>
     <p>Capitalised. You can use the acronym ‘DBS’ after the first full mention.</p>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
-    <h2 id="e">E</h2>
+    <h2 className="a-z-header" id="e">E</h2>
     <h3 id="eea biometric residence card">EEA biometric residence card</h3>
     <p>Lower case. Also called ‘UK residence card’ or ‘biometric residence card (BRC)’, these are no longer in use.</p>
     <h3 id="enhanced dbs check">enhanced DBS check</h3>
     <p>Use ‘enhanced DBS check’ rather than ‘enhanced disclosure’.</p>
     <h3 id="eu settlement scheme">EU Settlement Scheme (EUSS)</h3>
     <p>Capitalised. You can use the acronym ‘EUSS’ after the first full mention.</p>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
-    <h2 id="f">F</h2>
+    <h2 className="a-z-header" id="f">F</h2>
     <h3 id="finger scan">finger scan</h3>
     <p>Not a synonym for ‘fingerprint’. Fingers are ‘scanned’ and the results are called ‘fingerprints’.</p>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
-    <h2 id="g">G</h2>
-    <p>GOV.UK</p>
+    <h2 className="a-z-header" id="g">G</h2>
+    <h3 id="gov.uk">GOV.UK</h3>
     <p>All upper case.</p>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
-    <h2 id="h">H</h2>
+    <h2 className="a-z-header" id="h">H</h2>
     <h3 id="his majesty’s">His Majesty’s</h3>
     <p>Use ‘HM’ rather than ‘His Majesty’s’ in front of the name of the relevant public body. For example, ‘HM Revenue & Customs’.</p>
     <h3 id="his majesty’s passport office">His Majesty’s Passport Office</h3>
@@ -129,8 +134,9 @@ const Page: FC<PageProps> = ({ location }) => (
     <p>Use lower case for ‘reference number’.</p>
     <h3 id="horizon news">Horizon News</h3>
     <p>Capitalised.</p>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
-    <h2 id="i">I</h2>
+    <h2 className="a-z-header" id="i">I</h2>
     <h3 id="immigration bail">immigration bail</h3>
     <p>Lower case.</p>
     <h3 id="immigration health surcharge">immigration health surcharge (IHS)</h3>
@@ -143,14 +149,17 @@ const Page: FC<PageProps> = ({ location }) => (
     <p>Read our <A href="https://design.homeoffice.gov.uk/accessibility/inclusive-language">guidance on using inclusive language</A> in the Home Office design system.</p>
     <h3 id="indefinite leave to remain">indefinite leave to remain (ILR)</h3>
     <p>'Indefinite leave to remain' is officially called 'settlement' or 'settled status'. For example, 'apply for citizenship if you have settled status'.</p>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
-    <h2 id="j">J</h2>
+    <h2 className="a-z-header" id="j">J</h2>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
-    <h2 id="k">K</h2>
+    <h2 className="a-z-header" id="k">K</h2>
     <h3 id="king’s speech">King’s Speech</h3>
     <p>Capitalised.</p>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
-    <h2 id="l">L</h2>
+    <h2 className="a-z-header" id="l">L</h2>
     <h3 id="leave to enter">Leave to enter</h3>
     <p>Use 'permission to enter' instead of 'leave to enter', which is no longer in official use. For example, 'apply for permission to enter the UK'.</p>
     <h3 id="leave to remain">Leave to remain</h3>
@@ -159,18 +168,22 @@ const Page: FC<PageProps> = ({ location }) => (
     <p>Capitalise the ‘L’ and ‘T’. After first mention, you can refer to ‘the test’, using lower case.</p>
     <h3 id="log in">log in</h3>
     <p>Use ‘sign in’ rather than ‘log in’, ‘login’ or ‘logon’.</p>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
-    <h2 id="m">M</h2>
+    <h2 className="a-z-header" id="m">M</h2>
     <h3 id="migrant">migrant</h3>
     <p>Do not use ‘migrants’. Try to be specific, such as ‘applicant’, ‘claimant’, ‘employee’ or ‘worker’. If you cannot be specific, use ‘people’.</p>
     <h3 id="ministers">ministers</h3>
     <p>See guidance on using titles in the <A href="https://www.gov.uk/guidance/style-guide/a-to-z-of-gov-uk-style">GOV.UK style guide.</A></p>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
-    <h2 id="n">N</h2>
+    <h2 className="a-z-header" id="n">N</h2>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
-    <h2 id="o">O</h2>
+    <h2 className="a-z-header" id="o">O</h2>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
-    <h2 id="p">P</h2>
+    <h2 className="a-z-header" id="p">P</h2>
     <h3 id="passport control">passport control</h3>
     <p>Use ‘border control’ rather than ‘passport control’ or ‘primary control point’.</p>
     <h3 id="people">people</h3>
@@ -191,20 +204,23 @@ const Page: FC<PageProps> = ({ location }) => (
     <p>Use ‘border control’ rather than ‘passport control’ or ‘primary control point’.</p>
     <h3 id="purdah">purdah</h3>
     <p>Use ‘pre-election period’ rather than ‘purdah’.</p>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
-    <h2 id="q">Q</h2>
+    <h2 className="a-z-header" id="q">Q</h2>
     <h3 id="quotes and speech marks">quotes and speech marks</h3>
     <p>See guidance on quotes and speech marks in the <A href="https://www.gov.uk/guidance/style-guide/a-to-z-of-gov-uk-style">GOV.UK style guide</A>.</p>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
-    <h2 id="r">R</h2>
+    <h2 className="a-z-header" id="r">R</h2>
     <h3 id="rank">rank (police and armed forces)</h3>
     <p>Capitalise the rank before the name, as in ‘Detective Inspector Joan Smith’. Use lower case when it follows the name, as in ‘Joan Smith, a detective inspector’.</p>
     <h3 id="register">register</h3>
     <p>Use ‘create an account’ rather than ‘register’ or ‘set up’ an account.</p>
     <h3 id="right">right</h3>
     <p>Use ‘correct’ rather than ‘right’. For example, ‘Select the correct settlement route’.</p>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
-    <h2 id="s">S</h2>
+    <h2 className="a-z-header" id="s">S</h2>
     <h3 id="secondary control">secondary control</h3>
     <p>Use ‘customs’ rather than ‘secondary control’. For example, ‘customs regulations’ or ‘customs allowances’.</p>
     <h3 id="select">select</h3>
@@ -221,30 +237,38 @@ const Page: FC<PageProps> = ({ location }) => (
     <p>Avoid using ‘sorry’. See guidance in <A href="https://www.gov.uk/service-manual/design/writing-for-user-interfaces">Writing for user interfaces</A> on GOV.UK.</p>
     <h3 id="sovereign base areas">Sovereign Base Areas</h3>
     <p>Capitalised. You can use ‘SBA’ after the first full mention.</p>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
-    <h2 id="t">T</h2>
+    <h2 className="a-z-header" id="t">T</h2>
     <h3 id="thank you">thank you</h3>
     <p>Use ‘thank you’ sparingly. See guidance in <A href="https://www.gov.uk/service-manual/design/writing-for-user-interfaces">Writing for user interfaces</A> on GOV.UK.</p>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
-    <h2 id="u">U</h2>
+    <h2 className="a-z-header" id="u">U</h2>
     <h3 id="uk residence card">UK residence card</h3>
     <p>Lower case. Also called a ‘biometric residence card (BRC)’, these are no longer in use.</p>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
-    <h2 id="v">V</h2>
+    <h2 className="a-z-header" id="v">V</h2>
     <h3 id="visa">visa</h3>
     <p>Use ‘visa’ rather than technical terms like ‘route’ or ‘permission to enter’ in public-facing services, unless you need to be specific. For example, use ‘graduate visa’ rather than ‘graduate route’.</p>
     <h3 id="visa application centre">visa application centre (VAC)</h3>
     <p>Lower case. You can use ‘VAC’ after the first full mention. Do not use ‘visa section’, ‘visa processing post’ or ‘visa issuing office’.</p>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
-    <h2 id="w">W</h2>
+    <h2 className="a-z-header" id="w">W</h2>
     <h3 id="watchlist">watchlist</h3>
     <p>Lower case and one word.</p>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
-    <h2 id="x">X</h2>
+    <h2 className="a-z-header" id="x">X</h2>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
-    <h2 id="y">Y</h2>
+    <h2 className="a-z-header" id="y">Y</h2>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
 
-    <h2 id="z">Z</h2>
+    <h2 className="a-z-header" id="z">Z</h2>
+    <a className="back-to-top" href="#a-z-nav">Back to top</a>
   </Fragment>
 );
 
