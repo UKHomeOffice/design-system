3c6e7ac6562bfc81ce48bffe9cda56b6302324b8
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2023-10-05 16:14:33 +0100

    refactor timeout modal

--- components/timeout-modal/src/TimeoutModal.tsx
+++ components/timeout-modal/src/TimeoutModal.tsx
@@ -1,4 +1,4 @@
-import { FC, createElement as h, useState } from 'react';
+import { FC, createElement as h, useState, useEffect, useRef } from 'react';
 import { StandardProps, classBuilder } from '@not-govuk/component-helpers';
 
 import '../assets/TimeoutModal.scss';
@@ -13,6 +13,8 @@ export type TimeoutModalProps = StandardProps & {
   onContinue: () => void,
   /** Function called when the user clicks the modal link */
   onSignout: () => void,
+  /** Function called when timeout timer ends */
+  onTimeout: () => void
 };
 
 export const TimeoutModal: FC<TimeoutModalProps> = ({
@@ -20,25 +22,40 @@ export const TimeoutModal: FC<TimeoutModalProps> = ({
   classBlock,
   classModifiers,
   className,
-  isOpen,
-  timerDurationInSeconds = 300,
+  isOpen = true,
+  timerDurationInSeconds = 20,
   onContinue,
   onSignout,
+  onTimeout,
   ...attrs
 }) => {
   const classes = classBuilder('hods-timeout-modal', classBlock, classModifiers, className);
+  const modalRef = useRef();
+
+  // Taken from React documentation for managing modals with useEffect
+  useEffect(() => {
+    if(!isOpen) {
+      return;
+    }
+    const dialog = modalRef.current;
+    // @ts-ignore
+    dialog.showModal();
+    // @ts-ignore
+    return () => dialog.close();
+  }, [isOpen]);
 
   return (
-    <div {...attrs} className={classes('overlay')}>
-      <div role='dialog' aria-labelledby='modalTitle' aria-describedby='modalContent' className={classes('content')}>
-        <h1 id='modalTitle'>You will be signed out soon</h1>
-        <p id='modalContent' aria-live='polite'>To protect your information, you will be signed out in <Timer className={classes('timer')} timerFrom={timerDurationInSeconds}/>.</p>
+      <dialog ref={modalRef} aria-labelledby='modalTitle' aria-describedby='modalContent' className={classes('content')}>
+        <div {...attrs} className={classes('overlay')} />
+        <h2 id='modalTitle'>You will be signed out soon</h2>
+        <p id='modalContent' aria-live='polite'>
+          To protect your information, you will be signed out in <Timer className={classes('timer')} timerFrom={isOpen ? timerDurationInSeconds : 0} onTimeout={onTimeout}/>.
+        </p>
         <div className={classes('buttons')}>
           <button role='button' onClick={onContinue}>Continue on this page</button>
-          <a role='link' onClick={onSignout}>Sign out</a>  
+          <a role='link' onClick={onSignout}>Sign out</a>
         </div>
-      </div>
-    </div>
+      </dialog>
   );
 };
 
--- components/timeout-modal/src/Timer.tsx
+++ components/timeout-modal/src/Timer.tsx
@@ -3,49 +3,53 @@ import { StandardProps } from '@not-govuk/component-helpers';
 
 export type TimerProps = StandardProps & {
     /** Timeout starting value in seconds */
-    timerFrom: number
+    timerFrom: number,
+    /** Function called when timeout timer ends */
+    onTimeout: () => void
   };
 
 export const Timer: FC<TimerProps> = ({
     timerFrom,
+    onTimeout,
     ...attrs
 }) => {
     const [runTimer, setRunTimer] = useState(true)
     const [timer, setTimer] = useState(timerFrom);
-    const [formattedTimer, setFormattedTimer] = useState(Math.floor(timer / 60));
+    let formattedTimer = timer < 60 ? Math.ceil(timer / 20) * 20 : Math.round(timer / 60);
 
     useEffect(() => {
-      let interval;
-      if(runTimer) {
-        interval = setInterval(() => {
-          setTimer((timer) => timer - 1);
-        }, 1000);
-      } else {
-        clearInterval(interval);
-      }
-      return () => clearInterval(interval);
+        let interval;
+        if(runTimer) {
+            interval = setInterval(() => {
+                setTimer((timer) => timer - 1);
+            }, 1000);
+        } else {
+            clearInterval(interval);
+        }
+        return () => {
+            if(runTimer) setRunTimer(false);
+            clearInterval(interval);
+        };
     }, [runTimer])
-    
-    useEffect(() => {
-      if(timer > 60) {
-        if(timer % 60 == 0) {
-          setFormattedTimer(Math.round(timer / 60));
-        }
-      } else {
-        if([60,40,20,0].includes(timer)) {
-          setFormattedTimer(timer);
-          if(timer == 0) {
-            setRunTimer(false);
-          }
-        }
-      }
-    }, [timer])
 
-    useEffect(() => {
-      return () => {
-        setRunTimer(false);
-      };
-    }, []);
+    const formatTimer = (timer) => {
+        if(timer > 60) {
+            if(timer % 60 == 0) {
+                formattedTimer = Math.round(timer / 60);
+            }
+        } else {
+            if([60,40,20,0].includes(timer)) {
+                formattedTimer = timer;
+                if(timer == 0) {
+                    setRunTimer(false);
+                    onTimeout;
+                }
+            }
+        }
+    }
+    if(timer > 0) {
+      formatTimer(timer);
+    }
 
     return (
       <span {...attrs}>{formattedTimer} {timer <= 60 ? "seconds" : formattedTimer == 1 ? "minute" : "minutes"}</span>
