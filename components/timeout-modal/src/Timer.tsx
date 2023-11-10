9482e640723bd99f0c68d5f792beb0920fb4a4cb
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2023-11-10 12:42:03 +0000

    changes to fix doc rendering issue

--- components/timeout-modal/src/Timer.tsx
+++ components/timeout-modal/src/Timer.tsx
@@ -13,18 +13,16 @@ export const Timer: FC<TimerProps> = ({
     onTimeout,
     ...attrs
 }) => {
-    const [runTimer, setRunTimer] = useState(true)
     const [timer, setTimer] = useState(timerFrom);
     let formattedTimer = timer < 61 ? Math.ceil(timer / 20) * 20 : Math.ceil(timer / 60);
     const interval = useRef(null);
 
     useEffect(() => {
-        if(runTimer) interval.current = setInterval(() => setTimer((timer) => timer - 1), 1000);
-        else clearInterval(interval.current);
+        interval.current = setInterval(() => setTimer((timer) => timer - 1), 1000);
         return () => clearInterval(interval.current);
-    }, [runTimer])
+    }, [])
 
-    if(runTimer && timer == 0) {
+    if(timer == 0) {
         clearInterval(interval.current);
         onTimeout();
     }
