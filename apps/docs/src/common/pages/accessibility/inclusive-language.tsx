b1ba8897ffed26f6ecac0a4fbdf185bc43e2bf76
Steve Shukor <101883702+sshukor@users.noreply.github.com>
2024-02-19 14:24:13 +0000

    Footer replaced
    Replaced existing footer with:
    <h2>Help improve this guidance</h2>
        <p>To contribute, send your request to <A href="mailto:design@digital.homeoffice.gov.uk">design@digital.homeoffice.gov.uk,</A> or add it to the <A href="https://github.com/UKHomeOffice/design-system/discussions/470">GitHub discussion</A>.</p>

--- apps/docs/src/common/pages/accessibility/inclusive-language.tsx
+++ apps/docs/src/common/pages/accessibility/inclusive-language.tsx
@@ -130,10 +130,9 @@ const Page: FC<PageProps> = ({ location }) => (
                     <p>Use our quick reference <A href="https://github.com/UKHomeOffice/posters/tree/master/design/Inclusive%20language">inclusive language posters</A> as best practice guidelines while you design and research.</p>
                     <p>Download the posters for your office space and help colleagues think about how to write inclusively.</p>
 
-<div className="contact-us">
-  <h2 className="govuk-heading-m">Get in touch</h2>
-  <p>If you’ve got a question or suggestion share it on the Home Office DDaT Slack channel #inclusion.</p>
-</div>
+<h2>Help improve this guidance</h2>
+    <p>To contribute, send your request to <A href="mailto:design@digital.homeoffice.gov.uk">design@digital.homeoffice.gov.uk,</A> or add it to the <A href="https://github.com/UKHomeOffice/design-system/discussions/470">GitHub discussion</A>.</p>
+
 
     </div>
   </div>
