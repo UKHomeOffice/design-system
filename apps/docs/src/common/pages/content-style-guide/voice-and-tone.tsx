3ca0f9cbd390d472574d7de7afecee503031fc7d
Sarah Lombardi <39190312+Sarah-Lombardi@users.noreply.github.com>
2024-08-30 11:58:46 +0100

    Update voice-and-tone.tsx
    Added examples

--- apps/docs/src/common/pages/content-style-guide/voice-and-tone.tsx
+++ apps/docs/src/common/pages/content-style-guide/voice-and-tone.tsx
@@ -26,7 +26,7 @@ const Page: FC<PageProps> = ({ location }) => (
           {title}
       </h1>
       <h2 id="home-office-voice">Home Office voice</h2>
-      <p>Our voice should be consistent across all our products and services. It is who we are and how we want others to see us.</p>
+      <p>Our voice should be consistent across all our products and services. It is who we are and how we demonstrate our values and standards.</p>
 <p>A consistent voice presents us as trustworthy, confident and organised.</p>
 <p>The Home Office voice is:</p>
         <ul>
@@ -38,14 +38,34 @@ const Page: FC<PageProps> = ({ location }) => (
 <p>It is based on our core Home Office values of compassion, courage, respect and collaboration.</p>
 <p>Here are some examples showing what we mean.</p>
  <h3 id="human">Human</h3>
-<p>Be empathetic, natural, and use words that the audience understands. Vary your sentence length and structure. Do not be formulaic or monotonous and try to avoid standard templated text if you can.</p>
-<h3 id="empowering">Empowering</h3>
-<p>Focus on what someone can do, rather than what they cannot do or what will be done to them. Provide next steps. Speak to the reader, use ‘you’ as much as you can, and write in the active voice.</p>
+<p>A 'human' voice is empathetic, personal, and natural. It use words that the audience understands. Varying your sentence length and structure will help to make your content seem less rigid and monotonous. Try to avoid large amounts of templated text, unless the user needs it.</p>
+<p>For example:</p>
+      <ul>
+      <li> Use 'A family permit lets you come to the UK for up to 6 months. You can work and study, and come and go as many times as you want before the permit expires.</li>
+      <li> Instead of 'A family permit grants 6-month UK authorisation. It allows employment, education and unlimited entry/exit until expiration.'</li>
+      </ul>
+</p><h3 id="empowering">Empowering</h3>
+<p>Using an 'empowering' voice means focussing on what someone can do, rather than what they cannot do or what will be done to them. Provide next steps. Speak to the reader, use ‘you’ as much as you can, and write in the active voice.</p>
+<p>For example:</p>
+      <ul>
+      <li> Use '</li>
+      <li> Not '</li>
+      </ul>
 <h3 id="respectful">Respectful</h3>
-<p>Be inclusive, considerate and sensitive. A respectful voice is not accusing or demanding; it fosters an environment where people feel valued and treated with dignity.</p>
+<p>A 'respectful' voice is inclusive, considerate and sensitive. It is not accusing or demanding; it fosters an environment where people feel valued and treated with dignity.</p>
+<p>For example:</p>
+      <ul>
+      <li> Use '</li>
+      <li> Not '</li>
+      </ul>
 <h3 id="clear">Clear</h3>
 <p>Use plain and unambiguous language. Write to be understood. Be honest about what we are doing and why, and clarify and explain if needed. Do not hide meaning behind legal or government jargon.</p>
-
+<p>For example:</p>
+      <ul>
+      <li> Use '</li>
+      <li> Not '</li>
+      </ul>
+    
 <h2 id="tone">Tone</h2>
 <p>Unlike voice, tone can change depending on the audience or channel. Like in conversation, this is about being responsive to the person and the context.</p>
 <p>Here are some examples:</p>
