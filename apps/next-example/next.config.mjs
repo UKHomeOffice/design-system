15e4b2802ac3f2494786f1ef0802777c410408b3
Daniel A.C. Martin <github@daniel-martin.co.uk>
2024-12-12 17:14:48 +0000

    next-example: Update to new interface and clean-up

--- apps/next-example/next.config.mjs
+++ apps/next-example/next.config.mjs
@@ -4,15 +4,12 @@ import webpack from 'webpack';
 const nextConfig = {
   // This allows us to handle next-example, the same way we do standard apps, in CI
   distDir: 'dist',
-  // Prevent react-router from resolving in order to use Next's router instead.
-  // Normally users will not need to do this, as they will not have react-router installed.
   webpack: (config, _options) => ({
     ...config,
     plugins: [
       ...config.plugins,
-      new webpack.NormalModuleReplacementPlugin(/^react-helmet-async$/, '@not-govuk\/VOID-react-helmet-async'),
-      new webpack.NormalModuleReplacementPlugin(/^react-router$/, '@not-govuk\/VOID-react-router'),
-      new webpack.NormalModuleReplacementPlugin(/^react-router-dom$/, '@not-govuk\/VOID-react-router-dom'),
+      new webpack.NormalModuleReplacementPlugin(/^@not-govuk\/head$/, '@not-govuk\/head\/dummy'),    // ADD THIS LINE
+      new webpack.NormalModuleReplacementPlugin(/^@not-govuk\/router$/, '@not-govuk\/router\/next'), // ADD THIS LINE
     ]
   })
 };
