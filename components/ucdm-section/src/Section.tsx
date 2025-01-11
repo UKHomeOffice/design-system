c4187f46c0be018c9917a1d5a58c9a1c0a909c0a
Daniel A.C. Martin <github@daniel-martin.co.uk>
2025-01-11 15:04:27 +0000

    Add Section component

---
+++ components/ucdm-section/src/Section.tsx
@@ -0,0 +1,48 @@
+import { FC, ReactNode,  createElement as h } from 'react';
+import { StandardProps, classBuilder } from '@not-govuk/component-helpers';
+import { PageBanner } from '@hods/ucdm-page-banner';
+import { ServiceNavigation, ServiceNavigationProps } from '@hods/service-navigation';
+
+import '../assets/Section.scss';
+
+export type SectionProps = StandardProps & {
+  children?: ReactNode
+  /** Description of the section  */
+  description?: ReactNode
+  /** Links for the navigation  */
+  navigation?: Pick<ServiceNavigationProps, 'items'>['items']
+  /** Title of the section  */
+  title: string
+  /** URL of the section  */
+  href: string
+};
+
+export const Section: FC<SectionProps> = ({
+  children,
+  classBlock,
+  classModifiers,
+  className,
+  description = null,
+  navigation = [],
+  title,
+  href,
+  ...attrs
+}) => {
+  const classes = classBuilder('hods-ucdm-section', classBlock, classModifiers, className);
+
+  return (
+    <div {...attrs} className={classes()}>
+      <PageBanner className={classes('banner')} heading={title}>{description}</PageBanner>
+      { !navigation.length ? null : (
+        <ServiceNavigation className={classes('navigation')} items={navigation} serviceName={title} serviceHref={href} />
+      ) }
+      <div className={classes('container', undefined, 'hods-width-container')}>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+Section.displayName = 'Section';
+
+export default Section;
