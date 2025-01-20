dc21ebfe0d5e7cdd8999e056c033fcd53b6c383e
Daniel A.C. Martin <github@daniel-martin.co.uk>
2025-01-20 17:18:38 +0000

    Subsection: Make side-bar flexible

--- components/ucdm-subsection/src/Subsection.tsx
+++ components/ucdm-subsection/src/Subsection.tsx
@@ -1,13 +1,12 @@
 import { FC, ReactNode, createElement as h } from 'react';
 import { StandardProps, classBuilder } from '@not-govuk/component-helpers';
-import { NavigationMenu, NavigationMenuProps } from '@not-govuk/navigation-menu';
 
 import '../assets/Subsection.scss';
 
 export type SubsectionProps = StandardProps & {
   children?: ReactNode
-  /** List of links for the side navigation */
-  navigation: Pick<NavigationMenuProps, 'items'>['items']
+  /** Elements to be placed in the side bar */
+  side?: ReactNode
   /** The title of the subsection */
   title: string
 };
@@ -17,25 +16,32 @@ export const Subsection: FC<SubsectionProps> = ({
   classBlock,
   classModifiers,
   className,
-  navigation,
+  side,
   title,
   ...attrs
 }) => {
   const classes = classBuilder('hods-ucdm-subsection', classBlock, classModifiers, className);
+  const main = (
+    <main id="main-content" className={classes('main')} role="main">
+      <span className={classes('caption', undefined, 'govuk-caption-xl')}>{title}</span>
+      {children}
+    </main>
+  );
 
-  return (
+  return ( side ? (
     <div {...attrs} className={classes() + ' govuk-grid-row'}>
       <div className="govuk-grid-column-one-quarter">
-        <NavigationMenu items={navigation} />
+        {side}
       </div>
       <div className="govuk-grid-column-three-quarters">
-        <main id="main-content" className={classes('main')} role="main">
-          <span className={classes('caption', undefined, 'govuk-caption-xl')}>{title}</span>
-          {children}
-        </main>
+        {main}
       </div>
     </div>
-  );
+  ) : (
+    <div {...attrs} className={classes()}>
+      {main}
+    </div>
+  ) );
 };
 
 Subsection.displayName = 'Subsection';
