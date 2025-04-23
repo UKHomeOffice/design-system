a78046aeb30a255d4de99f5caacd973b6eef34f9
Sarah Lombardi <39190312+Sarah-Lombardi@users.noreply.github.com>
2025-04-23 17:28:47 +0100

    Update voice-and-tone.tsx
    Updates to v2 after feedback from style council

--- apps/docs/src/common/pages/content-style-guide/voice-and-tone.tsx
+++ apps/docs/src/common/pages/content-style-guide/voice-and-tone.tsx
@@ -28,33 +28,60 @@ const Page: FC<PageProps> = () => (
         <span className="caption">{section}</span>
           {title}
       </h1>
+      <p>Voice and tone are essential parts of communication.</p>
+      <p>They play a crucial role in building identity and trust.</p>
+      <p>Voice is how we show who we are and is consistent across our products and services.</p>
+      <p>Tone adapts to the channel and user need.</p>
       <h2 id="home-office-voice">Home Office voice</h2>
-     <p>Our voice is human, empowering, respectful and clear.</p>
-      <p>It should be consistent across all our products and services. It is who we are and how we demonstrate our values of courage, compassion, respect and collaboration.</p>
-<p>Here are some examples showing what we mean.</p>
- <h3 id="human">Human</h3>
-<p>A 'human' voice is empathetic, personal, and natural. It uses words that the audience understands. Varying your sentence length and structure will help to make your content seem less rigid and monotonous. If you reuse templated text, make sure all the content is relevant to the user.</p>
-<p>For example: 'You can work and study, and come and go as many times as you want before the permit expires.' rather than 'It allows employment, education and unlimited entry/exit.'</p>
-<h3 id="empowering">Empowering</h3>
-<p>Using an 'empowering' voice means focussing on what someone can do, rather than what they cannot do or what will be done to them. It provides options, next steps and support. Speak to the reader, use ‘you’ as much as you can, and write in the active voice.</p>
-<p>For example: 'You can prove your English language ability by...' rather than 'You will be assessed on your English language ability'.</p>
-<h3 id="respectful">Respectful</h3>
-<p>A 'respectful' voice is inclusive, considerate and sensitive. It is not accusing or demanding; it fosters an environment where people feel valued and treated with dignity.</p>
-<p>For example: 'You may be asked questions about difficult topics but it is important that you explain what has happened to you and your family.' rather than 'Tell the interviewer everything or it can count against you.'</p>
-<h3 id="clear">Clear</h3>
-<p>Use plain language that the user will understand. Be honest about what we are doing and why, and clarify and explain if needed. Do not hide meaning behind legal or government jargon.</p>
-<p>For example: 'give your fingerprints and photo' rather than 'provide biometric information'.</p>
-
-    
+        <p>Our voice is:</p>
+        <ul class="govuk-list govuk-list--bullet">
+        <li>human</li>
+        <li>empowering</li>
+        <li>respectful</li>
+        <li>clear</li>
+        </ul>	
+      <p>Using this voice consistently across our products and services is one of the ways we demonstrate the Home Office values of courage, compassion, respect and collaboration.</p>
+   <table class="govuk-table">
+  <thead class="govuk-table__head">
+    <tr class="govuk-table__row">
+      <th scope="col" class="govuk-table__header">Voice</th>
+      <th scope="col" class="govuk-table__header">What this means</th>
+      <th scope="col" class="govuk-table__header">Example</th>
+    </tr>
+  </thead>
+  <tbody class="govuk-table__body">
+    <tr class="govuk-table__row">
+      <td>Human</td>
+      <td>Use natural language that the audience understands.</td>
+      <td>'You can work, study, and travel in and out of the UK with this permit.' rather than 'It allows employment, education, and unlimited entry/exit.'</td>
+    </tr>
+    <tr class="govuk-table__row">
+      <td>Empowering</td>
+      <td>Emphasise what the user can do, using ‘you’ and the active voice.</td>
+      <td>'You can prove your English language ability by...' rather than 'You will be assessed on your English language ability'.</td>
+    </tr>
+    <tr class="govuk-table__row">
+      <td>Respectful</td>
+      <td>Be inclusive, considerate, and sensitive, instead of accusing or demanding.</td>
+      <td>‘It is important that you explain what has happened to you and your family' rather than 'Tell the interviewer everything or it can count against you'.</td>
+    </tr>
+    <tr class="govuk-table__row">
+      <td>Clear</td>
+      <td>Use clear language and avoid hiding meaning behind jargon or legal language.</td>
+      <td>'Give your fingerprints and photo' rather than 'Provide biometric information'.</td>
+    </tr>
+  </tbody>
+</table>
 <h2 id="tone">Tone</h2>
-<p>Tone can change depending on the audience or channel. Like in conversation, this is about being responsive to the person and the context.</p>
-<p>Whatever tone we take, it should reflect our voice and values.</p>
-<p>For example:</p>
-      <ul>
-<li>our HR virtual assistant is more chatty, informal and varied in tone than our static HR guidance and policies</li>
-<li>our case management systems are direct, clear, professional and active in tone as staff need instructional and straightforward language that reflects their work processes and helps them to complete tasks quickly and accurately</li> 
+<p>Unlike voice, tone can change. We adapt it all the time in spoken communication, for example using a softer tone with children or a more formal tone with our manager.</p>
+<p>It will still be recognisable as the Home Office through the use of our voice, but it will suit the user need and channel.</p>
+<p>Here are some examples from our services:</p>
+      <ul class="govuk-list govuk-list--bullet">
+<li>For our case management system, we use a direct tone that focuses on tasks and action rather than explanation: for example, ‘You must check the applicant’s documents’.</li>
+<li>For our HR chat tool, we use a conversational but professional tone which is informal (but not over familiar), polite and helpful: for example, ‘Need to find a colleague’s role and team?’</li>
+<li>When inviting neurodiverse participants to research, we use an empathetic and reassuring tone to create psychological safety.</li>
+<li>For users downloading large datasets, we remind them of their data handling responsibilities in a clear and concise tone, using short sentences to minimise the cognitive load.</li>
       </ul>
-      
       <h2>Help improve this guidance</h2>
       <p>To contribute, join the <A href="https://github.com/UKHomeOffice/design-system/discussions/600">GitHub discussion</A>.</p>
     </div>
