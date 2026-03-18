a0101b0b49c147c4243ab338e214738e9bb233a0
willrichards <williamrichards@hotmail.co.uk>
2026-03-18 08:54:02 +0000

    Update accessibility section to fix broken links

--- apps/docs/src/common/pages/accessibility/colour-and-contrast.tsx
+++ apps/docs/src/common/pages/accessibility/colour-and-contrast.tsx
@@ -4,7 +4,7 @@ import { Navigate } from 'react-router-dom';
 import { PageProps } from '@not-govuk/app-composer';
 import { A } from '@not-govuk/components';
 
-const newHref = '/accessibility/providing-alternatives/colour-and-constrast';
+const newHref = '/accessibility/providing-alternatives/colour-and-contrast';
 const newURL = 'https://design.homeoffice.gov.uk' + newHref;
 
 const Page: FC<PageProps> = () => (
